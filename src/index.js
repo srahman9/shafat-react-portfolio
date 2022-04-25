4362bc77614d6c642700052ed2cc779a4f55ed58
srahman9 <32348687+srahman9@users.noreply.github.com>
2022-04-25 17:29:32 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<App/>);
+
+reportWebVitals();

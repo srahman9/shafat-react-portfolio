6d920a0b0c38d3d084355b457cea631212a9f4aa
srahman9 <32348687+srahman9@users.noreply.github.com>
2022-04-25 17:26:53 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App/>);
